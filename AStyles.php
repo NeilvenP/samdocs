67d3ffe1657b12e0fb8c437058fb4a9392209c0e
NeilvenP <neilvencuteprincep@gmail.com>
2021-07-03 10:32:00 +0800

    first commit

---
+++ AStyles.php
@@ -0,0 +1,26 @@
+body {
+ background-image: linear-gradient( to left, white, cyan);
+}
+h1 {
+	font-family:verdana;
+  color: maroon;
+  text-align: center; 
+}
+p {
+  color: black;
+font-family:verdana;
+text-align: center; 
+}
+h5 {
+	font-family:Century Gothic;
+  color: maroon;
+  text-align: center; 
+  font-size: 25px;
+}
+
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+
---
+++ about.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head >
+	  <link rel="stylesheet" href="AStyles.css">
+
+<title>NP Ultimate</title>
+</head>
+<body  style="background-color:powderblue; style="text-align:center; >
+
+<h1 >NP Favorite Boy Groups</h1>
+<p>Boy groups under Bighit Labels</p>
+
+<h5> TXT, BTS and ENHYPEN</h5>
+<p> from the left TXT, debuted on 2019 under Bighit entertainment<br/> on the middle is BTS, a senior group who debuted in 2013<br/> and ENHYPEN on the right which debuted on 2020 from the survival show I-Land<br/> 
+TXT and BTS is manage by Bighit Entertainment while Enhypen is manage by Be:lift which is both under Bighit Labels <br/>
+TXT is composed of Yeonjun, Soobin, Beomgyu, Taehyun and Hueningkai<br/>
+BTS is composed of Namjoon, Seokjin, Yoongi, Hoseok, Jimin, V and Jungkook <br/>
+ENHYPEN is composed of Heeseung, Sunghoon, Ni-ki, Jay, Jake Jungwon and SUnoo </p>
+<img src="https://cdn.filestackcontent.com/LashUSZBSmysivRU8IAA/convert?cache=true&crop=0%2C0%2C1920%2C960&crop_first=true&quality=90&w=1920" alt="BTS" class="center" width="900" height="450">
+
+</body>
+</html>
